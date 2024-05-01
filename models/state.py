c59c4415f6df95474d594a588573b3108fc70031
elvoceouma <elviceoumaonyango@gmail.com>
2024-05-01 14:15:07 +0300

    pursing key word arguments as conditional args

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+State class, a subclass of BaseModel
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.engine import file_storage
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    A subclass of BaseModel class
+
+    Public class attribute:
+    name (str): state name
+    """
+
+    # name = ""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete')
+
+    @property
+    def cities(self):
+        all_cities = file_storage.FileStorage.all(self)
+        return all_cities
